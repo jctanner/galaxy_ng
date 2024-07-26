3704c4b9fa5ae204de15e7aa0469e6f3cf16ac11
James Tanner <tanner.jc@gmail.com>
2024-07-26 19:32:38 -0400

    Add dab's pagination class.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/app/api/ui_v2/views.py
+++ galaxy_ng/app/api/ui_v2/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import action
 from django_filters.rest_framework import DjangoFilterBackend
 
 from ansible_base.resource_registry.models import Resource
+from ansible_base.rest_pagination.default_paginator import DefaultPaginator
 
 from .filters import UserResourcesViewFilter
 from .models import UserResourcesView
@@ -15,3 +16,4 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = UserResourcesViewFilter
+    pagination_class = DefaultPaginator
