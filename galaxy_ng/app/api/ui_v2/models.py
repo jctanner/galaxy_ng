a3c67b4c61246c58e674d95ff882932c0469b0f0
James Tanner <tanner.jc@gmail.com>
2024-07-26 19:05:16 -0400

    Dead code.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/app/api/ui_v2/models.py
+++ galaxy_ng/app/api/ui_v2/models.py
@@ -6,8 +6,6 @@ User = get_user_model()
 
 
 class UserResourcesView(models.Model):
-    user_id = models.CharField(max_length=255, primary_key=True)
-    resource_id = models.CharField(max_length=255, primary_key=True)
 
     user = models.ForeignKey(User, to_field='id', db_column='user_id', on_delete=models.DO_NOTHING)
     resource = models.ForeignKey(Resource, to_field='id', db_column='resource_id', on_delete=models.DO_NOTHING)
