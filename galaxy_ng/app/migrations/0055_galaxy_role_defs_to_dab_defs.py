b1316c214fe51b5c4ca94cab363ace7de4f569b3
Alan Rominger <arominge@redhat.com>
2024-07-29 15:15:13 -0400

    Fill in some example data for DAB RBAC migration

--- galaxy_ng/app/migrations/0055_galaxy_role_defs_to_dab_defs.py
+++ galaxy_ng/app/migrations/0055_galaxy_role_defs_to_dab_defs.py
@@ -1,25 +1,34 @@
 from django.db import migrations
 
+from awx.main.migrations._dab_rbac import migrate_to_new_rbac, create_permissions_as_operation, setup_managed_role_definitions
+
 
 def copy_permissions_to_role_definitions(apps, schema_editor):
     Permission = apps.get_model('auth', 'Permission')
-    RoleDefinition = apps.get_model('ansible_base.rbac', 'RoleDefinition')
+    RoleDefinition = apps.get_model('dab_rbac', 'RoleDefinition')
 
+    # TODO: migrate from pulp role model or something like that
     permissions = Permission.objects.all().filter(name__icontains='namespace')
-    for perm in permissions:
-        RoleDefinition.objects.create(
-            name=perm.name,
-            permissions=[perm.codename],
-            content_type=perm.content_type
+    rd = RoleDefinition.objects.get_or_create(
+        name='Namespace Admin',
+        defaults=dict(
+            content_type=permissions[0].content_type,
+            managed=True
         )
+    )
+    for perm in permissions:
+        rd.permissions.add(perm)
+
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('galaxy', '0054_dab_resource_views'),
+        ('dab_rbac', '__first__'),
     ]
 
     operations = [
-        # migrations.RunPython(copy_permissions_to_role_definitions),
+        migrations.RunPython(create_permissions_as_operation, migrations.RunPython.noop),
+        migrations.RunPython(copy_permissions_to_role_definitions),
     ]
